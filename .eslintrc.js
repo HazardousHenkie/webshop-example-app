66b9fa5c2702b2d9637275933bc05c498c1958ea
kyle-sun-asterisk <thekyle@hotmail.com>
2020-02-04 14:55:46 +0900

    change

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,13 +10,12 @@ module.exports = {
   extends: ['airbnb', 'prettier', 'prettier/react'],
   plugins: ['prettier', 'redux-saga', 'react', 'react-hooks', 'jsx-a11y'],
   env: {
-    jest: true,
     browser: true,
     node: true,
     es6: true
   },
   parserOptions: {
-    ecmaVersion: 6,
+    ecmaVersion: 8,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true
