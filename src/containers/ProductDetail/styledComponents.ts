0781b028bc9f0b70d96f9546869b1603ec6979d9
kyle-sun-asterisk <thekyle@hotmail.com>
2020-03-01 12:41:20 +0900

    styling and adding elements

--- src/containers/ProductDetail/styledComponents.ts
+++ src/containers/ProductDetail/styledComponents.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 import CardMedia from '@material-ui/core/CardMedia'
 import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
 
 export const CardMediaStyled = styled(CardMedia)`
   height: 0;
@@ -10,4 +11,9 @@ export const CardMediaStyled = styled(CardMedia)`
 
 export const IconButtonStyled = styled(IconButton)`
   margin-left: auto;
+`
+
+export const TypographyStyled = styled(Typography)`
+  font-size: 3rem;
+  margin: 30px 0 20px;
 `
